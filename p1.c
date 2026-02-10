e31898fa4fddff52c267c4e807e53d8fb7fe2e3d
Krishanayi <krishanayi123@gmail.com>
2026-02-10 18:47:58 +0530

    Implement basic arithmetic operations in p1.c

---
+++ p1.c
@@ -0,0 +1,17 @@
+/** Arithmetic Operations */
+#include<stdio.h>
+int main()
+{
+     int a;
+     int b;
+  printf("\n Enter the value of a and b");
+  scanf("%d %d", &a, &b);
+
+  printf("\n Add=%d", a+b);
+  printf("\n Sub=%d", a-b);
+  printf("\n Mul=%d", a*b);
+  printf("\n Div=%d", a/b);
+  printf("\n Rem=%d", a%b);
+
+return 0;
+}
